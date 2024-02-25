2eae93018609449df4a0d7d50bf2a2cbf1a13229
Vỹ Ngụy Như Đặng <nguynhudangvy5.10.1998@gmail.com>
2024-02-25 12:40:18 +0700

    create react app

---
+++ 03-frontend/react-library/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to full-stack project :-)</h1>
+    </div>
+  );
+}
+
+export default App;
