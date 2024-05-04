3a6b15656ec2396fb10ea25d9a65facee2f3bbfb
vynnd <nguynhudangvy5.10.1998@gmail.com>
2024-05-04 15:47:13 +0700

    BE: Create MessageEntity + MessageRepository + add Message + read message by userEmail

---
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/MessageController.java
@@ -0,0 +1,23 @@
+package com.luv2code.springbootlibrary.controller;
+
+import com.luv2code.springbootlibrary.entity.Message;
+import com.luv2code.springbootlibrary.service.MessageService;
+import com.luv2code.springbootlibrary.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+
+    @Autowired
+    private MessageService messageService;
+
+    @PostMapping("/secure/add/massage")
+    public void postMessage(@RequestHeader(value="Authorization") String token,
+                            @RequestBody Message messageRequest){
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        messageService.postMessage(messageRequest, userEmail);
+    }
+}
