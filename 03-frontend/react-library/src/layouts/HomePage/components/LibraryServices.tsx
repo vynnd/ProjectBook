0eb366ce09354b5a3206efdbb36d7c1866493fbb
vynnd <nguynhudangvy5.10.1998@gmail.com>
2024-05-04 15:48:32 +0700

    FE: MessagePage

--- 03-frontend/react-library/src/layouts/HomePage/components/LibraryServices.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/components/LibraryServices.tsx
@@ -18,7 +18,7 @@ export const LibraryServices = () => {
                     </p>
                     <div className='d-grid gap-2 justify-content-md-start mb-4 mb-lg-3'>
                         {authState?.isAuthenticated ?
-                            <Link type ='button' className='btn main-color btn-lg text-white' to='search'>
+                            <Link type ='button' className='btn main-color btn-lg text-white' to='/messages'>
                                 Library Services
                             </Link>
                         :
