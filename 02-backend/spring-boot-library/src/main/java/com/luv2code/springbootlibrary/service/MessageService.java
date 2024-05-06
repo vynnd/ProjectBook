ddfc2dfe419bc95ded392a0a6ca86ffa4bfb24c5
vynnd <nguynhudangvy5.10.1998@gmail.com>
2024-05-06 15:22:39 +0700

    BE: Function admin answers client question

--- 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/MessageService.java
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/MessageService.java
@@ -2,9 +2,11 @@ package com.luv2code.springbootlibrary.service;
 
 import com.luv2code.springbootlibrary.dao.MessageRepository;
 import com.luv2code.springbootlibrary.entity.Message;
+import com.luv2code.springbootlibrary.requestmodels.AdminQuestionRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -17,5 +19,16 @@ public class MessageService {
         Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
         message.setUserEmail(userEmail);
         messageRepository.save(message);
+    }
+
+    public void putMessage(AdminQuestionRequest adminQuestionRequest, String userEmail) throws Exception{
+        Optional<Message> message =  messageRepository.findById(adminQuestionRequest.getId());
+        if(!message.isPresent()){
+            throw new Exception("Message not found");
+        }
+        message.get().setAdminEmail(userEmail);
+        message.get().setResponse(adminQuestionRequest.getResponse());
+        message.get().setClosed(true);
+        messageRepository.save(message.get());
     }
 }
