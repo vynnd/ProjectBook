554b61d835830d5aedfa6e30af7764128b6699b7
vynnd <nguynhudangvy5.10.1998@gmail.com>
2024-05-07 14:06:01 +0700

    BE: change http -> https

--- 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/AdminController.java
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/AdminController.java
@@ -1,14 +1,12 @@
 package com.luv2code.springbootlibrary.controller;
 
-import com.luv2code.springbootlibrary.dao.CheckoutRepository;
-import com.luv2code.springbootlibrary.dao.ReviewRepository;
 import com.luv2code.springbootlibrary.requestmodels.AddBookRequest;
 import com.luv2code.springbootlibrary.service.AdminService;
 import com.luv2code.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://localhost:3000")
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
